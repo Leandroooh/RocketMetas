c85d525d2fa73b90a1d441d26807064d97ca318c
Leandro <leandrosilvadeoliveira0605@gmail.com>
2024-09-11 09:51:58 -0300

    important infos

---
+++ index.js
@@ -0,0 +1,19 @@
+let meta = {
+    value: `Ler um livro por mês`,
+    checked: true,
+};
+
+let metas = [
+    meta,
+    {
+        value: `Estudar por 30 minutos`,
+        checked: false
+    }
+];
+
+console.log(metas); 
+
+
+const createMeta = () => {
+
+}
