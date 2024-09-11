42aea2bd4f8d3626b4f2bbe7c4e2b95711d60e94
Leandro <leandrosilvadeoliveira0605@gmail.com>
2024-09-11 11:02:00 -0300

    list metas

--- index.js
+++ index.js
@@ -1,8 +1,88 @@
-const start = () => {
+// Desestruturação e Importação de módulos
+const { select, input, checkbox } = require('@inquirer/prompts');
+
+let metas = [];
+
+const createMeta = async () => {
+    const meta = await input({ message: "Digite sua meta!" });
+
+    if (meta.length == 0) {
+        console.log('Você não digitou nenhuma meta.');
+        return;
+    };
+
+    metas.push({ value: meta, checked: false });
+    console.log(`Meta cadastrada com sucesso: ${meta}`);
+};
+
+const listMeta = async () => {
+    if (metas.length == 0) {
+        console.log('Você não possui nenhuma meta cadastrada.');
+        return;
+    };
+
+    const answers = await checkbox({
+        message: 'Use as setas para se movimentar entre as metas, o espaço para marcar e/ou desmarcar e o enter para finalizar a meta',
+        choices: [...metas], // ... = "Duplicar os dados de Metas, sem alteração no inicial"
+    });
+
+    if(answers.length == 0) {
+        console.log('Você não marcou nenhuma meta.');
+        return;
+    }
+
+    answers.forEach((m) => {
+        m.checked = false;
+    })
+
+    answers.forEach((answer) => {
+        const meta = metas.find((m) => {
+            return m.value === answer;
+        });
+        meta.checked = true;
+    })
+}
+
+// async - Precisa esperar ?
+const start = async () => {
 
     while (true) {
-        console.log();
-        return; // Finaliza
+
+        // await - Espere
+        const option = await select({
+            message: '',
+            choices: [
+                {
+                    name: 'Cadastrar meta',
+                    value: 'cadastrar' // Valor que será utilizado no case
+                },
+                {
+                    name: 'Listar Metas',
+                    value: 'listar' // Valor que será utilizado no case
+                },
+                {
+                    name: 'Sair',
+                    value: 'sair' // Valor que será utilizado no case
+                },
+            ],
+        });
+
+        switch (option) {
+            case 'cadastrar':
+                await createMeta();
+                break;
+
+            case 'listar':
+                await listMeta();
+                break;
+
+            case 'sair':
+                console.log('Aguardamos seu retorno...')
+                return;
+        }
+
     }
 
 }
+
+start()
