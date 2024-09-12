3b27fa4655c9acd2d1e784f93158979581568c18
Leandro <leandrosilvadeoliveira0605@gmail.com>
2024-09-12 10:27:35 -0300

    fix

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Desestruturação e Importação de módulos
 const { select, input, checkbox } = require('@inquirer/prompts');
 
+let message = '';
+
 let metas = [];
 
 const createMeta = async () => {
@@ -60,13 +62,14 @@ const performedMeta = async () => {
         message: 'Metas Realizadas',
         choices: [...completed] // spread operator (...)
     });
-
 }
 
 const pendingMeta = async () => {
+
     const pending = metas.filter((m) => {
         return !m.checked;
     });
+
     if (pending.length == 0) {
         console.log('Não existem metas pendentes!');
         return;
@@ -76,15 +79,42 @@ const pendingMeta = async () => {
         message: 'Metas Pendentes',
         choices: [...pending] // spread operator (...)
     });
+
 }
 
 const deleteMeta = async () => {
 
-    const deleteMeta = await select({
-        message: 'Escolha a meta que deseja excluir',
-        choices: [...metas] // spread operator (...)
+    // desmarcando as metas, formatando o objeto.
+    const selectedMetas = metas.map((m) => {
+        return { value: m.value, checked: false };
+    })
+
+    const deleteItems = await checkbox({
+        message: 'Use as setas para se movimentar entre as metas, o espaço para marcar e/ou desmarcar e o enter para finalizar a meta',
+        // ... = "Duplicar os dados de Metas, sem alteração no inicial"
+        choices: [...selectedMetas],
     });
+
+    if (deleteItems.length == 0) {
+        console.log('Você não marcou nenhuma meta para deletar.');
+        return;
+    }
+
+    deleteItems.forEach((item) => {
+        // se verdadeiro passa e fica na lista do filtro
+        metas.filter((m) => {
+            // Andar ( Metas ) é diferente de andar (selecionado)? Deleta.
+            return m.value != item;
+        })
+    })
+
+    console.log(`Meta(s) deletada(s) com sucesso!`)
+
 }
+
+const createMessage = ({
+    c
+});
 
 // async - Precisa esperar ?
 const start = async () => {
